0a6d2ef1932a5efb3321ce5af6a9f49faf39f4a3
Jay Rainey <jawrainey@gmail.com>
2014-03-13 22:36:53 +0000

    Add some PEP8 improvements.

--- pysay.py
+++ pysay.py
@@ -22,20 +22,23 @@ if __name__ == "__main__":
   APIKEY = "1953c74c5b60c0e3bdabaeb822e4e14c"
 
   parser = argparse.ArgumentParser(prog='pySay', usage='%(prog)s [options]',
-      description='Pronounce a specified word or phrase in its native language.')
+    description='Pronounce a specified word or phrase in its native language.')
 
   parser.add_argument('word', help="A word or phrase to pronounce.")
-  parser.add_argument('-l','--lang', help="A two character language code, e.g. 'en' or 'ru'."
-      "(default: automatically inferred from word or phrase entered.)")
-  parser.add_argument('-s','--sex', help="The sex of the speaker pronouncing the specified word.")
-  parser.add_argument('-a','--action', default="standard-pronunciation",
-      help="Set to 'word-pronunciations' for varying audio examples, or 'standard-pronounciation' for top rated result." +
-      "(default: Top rated pronunciation of desired word)")
+  parser.add_argument('-l', '--lang',
+    help="A two character language code, e.g. 'en' or 'ru'."
+    "(default: automatically inferred from word or phrase entered.)")
+  parser.add_argument('-s', '--sex',
+    help="The sex of the speaker pronouncing the specified word.")
+  parser.add_argument('-a', '--action', default="standard-pronunciation",
+    help="Set to 'word-pronunciations' for varying audio examples, or"
+    "'standard-pronounciation' for top rated result."
+    "(default: Top rated pronunciation of desired word)")
 
   args = parser.parse_args()
 
-  url =  "http://apifree.forvo.com/limit/1/format/json/key/" + APIKEY
-  url += "/word/" + args.word #No need to validate as 'too few arguments' error is throw if no argument passed.
+  url = "http://apifree.forvo.com/limit/1/format/json/key/" + APIKEY
+  url += "/word/" + args.word
 
   if args.lang:
     url += "/language/" + args.lang
@@ -59,9 +62,9 @@ if __name__ == "__main__":
   tf = tempfile.NamedTemporaryFile()
 
   with open(tf.name, 'wb') as f:
-    shutil.copyfileobj(audioRequest,f)
+    shutil.copyfileobj(audioRequest, f)
 
   if platform.system() == 'Darwin':
-    subprocess.call(["afplay",tf.name])
+    subprocess.call(["afplay", tf.name])
   else:
-    subprocess.call(["mplayer","-really-quiet",tf.name])
+    subprocess.call(["mplayer", "-really-quiet", tf.name])
